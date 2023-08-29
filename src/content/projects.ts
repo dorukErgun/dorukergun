e2dab7eab2431af8d8f5db2e34d09b7bdfb72331
Doruk Ergün <dorukergun@Doruks-MacBook-Air-2.local>
2023-08-29 17:29:56 +0300

    Fix projects

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -8,25 +8,4 @@ export const PROJECTS : Project[] = [
     url: "wwww.munule.io",
     image: "/gradient1.png"
   },
-  {
-    name: "Menule",
-    description: "Menule is a web application for restaurant owners that wants to create fast, highly editable QR menus.",
-    technologyTags: ["React", "Typescript", "Node.js", "Express", "PostgreSQL", "Docker", "Kubernetes", "Jenkins"],
-    url: "wwww.munule.io",
-    image: "/gradient1.png"
-  },
-  {
-    name: "Menule",
-    description: "Menule is a web application for restaurant owners that wants to create fast, highly editable QR menus.",
-    technologyTags: ["React", "Typescript", "Node.js", "Express", "PostgreSQL", "Docker", "Kubernetes", "Jenkins"],
-    url: "wwww.munule.io",
-    image: "/gradient1.png"
-  },
-  {
-    name: "Menule",
-    description: "Menule is a web application for restaurant owners that wants to create fast, highly editable QR menus.",
-    technologyTags: ["React", "Typescript", "Node.js", "Express", "PostgreSQL", "Docker", "Kubernetes", "Jenkins"],
-    url: "wwww.munule.io",
-    image: "/gradient1.png"
-  },
 ] 
